da5b96342ca9a1c19eb347231ec8a6b31251cc99
Alex <almat@gmx.de>
2019-01-19 09:54:35 +0100

    Testing source tree.

--- source/main.js
+++ source/main.js
@@ -1,16 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var dijkstra_1 = require("./logic/algorithms/pathfinding/pathfinders/dijkstra");
-var map_loader_1 = require("./scene/world/map-loader");
-var pathing_manager_1 = require("./logic/pathing-manager");
-var vector_1 = require("./logic/math/vector");
 function main() {
-    var loader = new map_loader_1.default();
-    var map = loader.load("../public/assets/maps/default.map");
-    var manager = new pathing_manager_1.default(map);
-    var pathFinder = new dijkstra_1.default(map, manager.tools);
-    var distances = pathFinder.getComputed(new vector_1.default(0, 0));
-    console.log("Done!");
+    // let loader: MapLoader = new MapLoader();
+    // let map: Map = loader.load("../public/assets/maps/default.map");
+    // let manager: PathingManager = new PathingManager(map);
+    // let pathFinder: Dijkstra = new Dijkstra(map, manager.tools);
+    // let distances: object = pathFinder.getComputed(new Vector(0, 0));
+    // console.log("Done!");
 }
 // start program
 main();
--- source/main.ts
+++ source/main.ts
@@ -5,12 +5,12 @@ import PathingManager from "./logic/pathing-manager";
 import Vector from "./logic/math/vector";
 
 function main(): void {
-    let loader: MapLoader = new MapLoader();
-    let map: Map = loader.load("../public/assets/maps/default.map");
-    let manager: PathingManager = new PathingManager(map);
-    let pathFinder: Dijkstra = new Dijkstra(map, manager.tools);
-    let distances: object = pathFinder.getComputed(new Vector(0, 0));
-    console.log("Done!");
+    // let loader: MapLoader = new MapLoader();
+    // let map: Map = loader.load("../public/assets/maps/default.map");
+    // let manager: PathingManager = new PathingManager(map);
+    // let pathFinder: Dijkstra = new Dijkstra(map, manager.tools);
+    // let distances: object = pathFinder.getComputed(new Vector(0, 0));
+    // console.log("Done!");
 }
 
 // start program
